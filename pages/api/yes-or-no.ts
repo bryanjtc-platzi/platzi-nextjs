ed078eaed6e18f1686e5d54f13ae7162c853153a
Bryan Thomas <49354825+bryanjtc@users.noreply.github.com>
2021-08-09 01:23:46 -0500

    Fixes

--- pages/api/yes-or-no.ts
+++ pages/api/yes-or-no.ts
@@ -2,6 +2,8 @@ import { NextApiResponse, NextApiRequest } from 'next'
 import enablePublicAccess from '@cors'
 
 const allAvos = async (req: NextApiRequest, res: NextApiResponse) => {
+  // Generally, you would not want this in your apps.
+  // See more in 'cors.js'
   await enablePublicAccess(req, res)
 
   const answer = Math.round(Math.random()) ? 'yes' : 'no'
